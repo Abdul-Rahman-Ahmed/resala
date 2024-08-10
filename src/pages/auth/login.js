dc8e68e2c22571fe275f21df6c69740ed7db2363
Abdul-Rahman Ahmed <abdulrahman102034@gmail.com>
2024-08-11 00:31:25 +0300

    login design

---
+++ src/pages/auth/login.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./login.css";
+
+const Login = () => {
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("http://localhost:4000/api/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+      if (res.ok) {
+        const data = await res.json();
+        localStorage.setItem("authToken", data.token);
+        navigate("/main");
+      } else {
+        console.error("Login Failed");
+      }
+      setEmail("");
+      setPassword("");
+    } catch (err) {
+      console.error("error during:", err);
+    }
+  };
+  return (
+    <div className="login">
+      <form onSubmit={submitHandler}>
+        <div className="img">
+          <img src={require("../../assets/logo.png")} alt="logo" />
+        </div>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="البريد الالكتروني"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="كلمة المرور"
+        />
+        <button type="submit">الدخول</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/main.js
@@ -0,0 +1,68 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { edit, getVolunteers } from "../store/volunteers.slice";
+import { updateDate, updateId } from "../store/form";
+
+const Main = () => {
+  const disPatch = useDispatch();
+
+  useEffect(() => {
+    disPatch(getVolunteers());
+  }, [disPatch]);
+  let volunteers = useSelector((state) => state.volunteers.volunteers);
+  let id = useSelector((state) => state.form.id);
+  let date = useSelector((state) => state.form.date);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    disPatch(edit({ id, date }));
+    disPatch(updateId(""));
+  };
+  return (
+    <div className="App">
+      <form onSubmit={submitHandler}>
+        <div className="container">
+          <label>اسم المتطوع</label>
+          <input
+            list="names"
+            placeholder="ادخل اسم المتطوع"
+            value={id}
+            onChange={(e) => disPatch(updateId(e.target.value))}
+          />
+          <datalist id="names">
+            {volunteers.map(({ name, number }, key) => {
+              return (
+                name !== "فارغ" && (
+                  <option value={key + 1} key={key}>
+                    {name + " " + number}
+                  </option>
+                )
+              );
+            })}
+          </datalist>
+        </div>
+        <div className="container">
+          <label>تاريخ المشاركة</label>
+          <input
+            type="number"
+            min="1"
+            max="31"
+            value={date}
+            onChange={(e) => disPatch(updateDate(e.target.value))}
+          />
+        </div>
+        <button type="submit">مشاركة</button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.href =
+              "https://resala-backend.onrender.com/downloadSheet";
+          }}
+        >
+          تحميل الشيت
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Main;
